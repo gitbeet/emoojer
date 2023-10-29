3ae44bf057430ddeaa927bc9b8f82996ee87395b
gitbeet <nikig715@gmail.com>
2023-10-29 19:58:38 +0100

    bg mobile fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,9 +7,9 @@ const Layout = (props: PropsWithChildren) => {
   const { isSignedIn, user } = useUser();
   return (
     <main className="relative flex min-h-screen text-slate-100">
-      <div className="fixed inset-0 w-screen bg-slate-800"></div>
+      <div className="fixed inset-0 min-h-screen w-screen bg-slate-800"></div>
       <div className="relative z-20 mx-auto w-full  lg:w-[700px] lg:border-x-2 lg:border-slate-700  ">
-        <div className="absolute inset-0 z-[-1]  bg-[rgba(12,12,30,0.15)] "></div>
+        <div className="absolute inset-0 z-[-1] min-h-screen  bg-[rgba(12,12,30,0.15)] "></div>
         <nav className="flex items-center justify-between gap-8  border-b border-slate-700  px-8 py-6 ">
           <Link href="/">
             <Logo />
